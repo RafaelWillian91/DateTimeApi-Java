1a88bdc7ea419f52d17955786198239272e6228f
rafaw <rafa.willian91@gmail.com>
2023-04-05 21:30:00 -0300

    ZonedDateTime Trabalhando com Fuso Horario

--- src/PassandoUmZonedDateTime/ZonedDateTimeapp7.java
+++ src/PassandoUmZonedDateTime/ZonedDateTimeapp7.java
@@ -7,7 +7,7 @@ import java.time.ZonedDateTime;
 public class ZonedDateTimeapp7 {
 
     public static void main(String[] args) {
-
+        //ZonedDateTime é uma classe que representa uma data e hora com informações de fuso horário
         //ZonedDateTime permite eu passar uma zona especifica para configuracao da data e hora
         LocalDateTime localDateTime = LocalDateTime.of(2000, 01, 01, 13,0,0);
         ZonedDateTime ztd = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
@@ -27,5 +27,8 @@ public class ZonedDateTimeapp7 {
         ZonedDateTime znP = ZonedDateTime.of(LocalDateTime.now(), ZoneId.systemDefault());
         znP = znP.withZoneSameInstant(ZoneId.of("Europe/Lisbon"));
         System.out.println(znP);
+
+        //Isso é especialmente útil em aplicações que precisam trabalhar com horários em várias partes do mundo,
+        //como sistemas de reservas de viagens ou sistemas de comércio eletrônico com clientes em diferentes fusos horários.
     }
 }
